f95b3642a9cbfde0c7c5361437d7d8476f411469
Alexandre Ferreira <ferolialexandre@gmail.com>
2023-10-03 18:01:28 -0300

    Finalizando o módulo de eventos

--- modulo_javascript/010-DOM/006-eventos/001-manipulador-de-eventos/script.js
+++ modulo_javascript/010-DOM/006-eventos/001-manipulador-de-eventos/script.js
@@ -1,22 +1,75 @@
 "use strict";
 
 const btnOnClick = document.querySelector(".onclick");
+const btnEvent = document.querySelector(".event");
+const btnRemove = document.querySelector(".remove");
+const body = document.querySelector("body");
+const h3 = document.querySelector("h3");
+const link = document.querySelector("a");
+const acionarAlert = () => {
+    let validador = false;
+
+    alert("Alert do RemoveEventListenner");
+
+    validador = true;
+
+    if (validador) {
+        btnRemove.removeEventListener("click", acionarAlert);
+    }
+};
 
 btnOnClick.onclick = function alertOne() {
-    alert("Alerta vindo do script pelo evento OnClick.");
+    alert("Alerta do evento OnClick.");
 }
 
-const btnEvent = document.querySelector(".event");
-
-btnEvent.addEventListener('click', (event) => {
-    alert("Alerta vindo do script pelo addEventListener.");
+btnEvent.addEventListener("mouseover", (event) => {
+    body.style.background = "blue";
 });
 
-btnEvent.addEventListener('mouseover', (event) => {
-    btnEvent.style.background = "blue";
-    btnEvent.style.color = "white";
+btnEvent.addEventListener("mouseout", (event) => {
+    body.style.background = "#FFF";
 });
 
-btnEvent.addEventListener('mouseout', (event) => {
-    btnEvent.style.background = "red";
+btnEvent.addEventListener("click", (event) => {
+    alert("Alerta do EventListenner");
 });
+
+btnRemove.addEventListener("click", acionarAlert);
+
+link.addEventListener("click", (event) => {
+    event.preventDefault();
+    console.log("Evento prevenido!");
+});
+
+const menu = document.querySelector("#menu");
+
+menu?.addEventListener("click", (event) => {
+    const { target } = event;
+    const pd = document.querySelector(".preventing-default");
+
+    switch (target.getAttribute("class")) {
+        case "home":
+            pd.style.background = "red";
+            break;
+
+        case "sobre":
+            pd.style.background = "blue";
+            break;
+
+        case "contato":
+            pd.style.background = "orange";
+            break;
+
+        default:
+            break;
+    }
+});
+
+
+
+
+
+
+
+
+
