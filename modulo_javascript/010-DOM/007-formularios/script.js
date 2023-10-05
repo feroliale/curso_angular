fd14a4593f6818c9c511f3537223d1ffcf8e73c8
Alexandre Ferreira <ferolialexandre@gmail.com>
2023-10-05 12:16:15 -0300

    FInalizando o módulo de Javascript.

--- modulo_javascript/010-DOM/007-formularios/script.js
+++ modulo_javascript/010-DOM/007-formularios/script.js
@@ -1,36 +1,67 @@
 "use strict";
 
-const form = document.forms.namedItem("select-radio");
+const form = document.forms.namedItem("select-checkbox");
 const submit = document.querySelector('#submit');
 const change = document.querySelector('#change');
 
+let checkedValues = [];
+
 form.addEventListener("submit", (event) => {
     event.preventDefault();
-
-    const radio = form.radio;
-
-
-    submit.innerHTML = radio.value;
-
-    // console.log(radio.value);
-    // radio.forEach((res) => {
-    //     if (res.checked) {
-    //         console.log(res);
-    //     }
-    // });
+    submit.innerHTML = checkedValues;
 });
 
-form.radio.forEach(res => {
-    res.addEventListener('change', (event) => {
-        const { target } = event;
-
-        if (target.checked) {
-            change.innerHTML = target.value;
-        }
-
+form.checkbox.forEach((element) => {
+    element.addEventListener("change", (event) => {
+        hasChecked(event, element);
+        change.innerHTML = checkedValues;
     });
 });
 
+const hasChecked = (event, element) => {
+    const { target } = event;
+
+    if (target.checked) {
+        return checkedValues.push(element.value);
+    }
+
+    if (!target.checked) {
+        return checkedValues.map((checkedValue, index) => {
+            if (element.value === checkedValue) {
+                return checkedValues.splice(index, 1);
+            }
+        });
+    }
+};
+
+/* MANIPUALNDO RADIOBUTTONS
+// form.addEventListener("submit", (event) => {
+//     event.preventDefault();
+
+//     const radio = form.radio;
+
+
+//     submit.innerHTML = radio.value;
+
+//     // console.log(radio.value);
+//     // radio.forEach((res) => {
+//     //     if (res.checked) {
+//     //         console.log(res);
+//     //     }
+//     // });
+// });
+
+// form.radio.forEach(res => {
+//     res.addEventListener('change', (event) => {
+//         const { target } = event;
+
+//         if (target.checked) {
+//             change.innerHTML = target.value;
+//         }
+
+//     });
+// });
+ */
 
 /* MANIPULANDO INPUT
 // const form = document.forms.namedItem("registration");
