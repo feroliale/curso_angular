61056ae8fb4b2076fb4774027591b63a96f41b96
Alexandre Ferreira <ferolialexandre@gmail.com>
2023-10-05 11:46:46 -0300

    Manipulação de eventos utilizando radio buttons.

--- modulo_javascript/010-DOM/007-formularios/script.js
+++ modulo_javascript/010-DOM/007-formularios/script.js
@@ -1,25 +1,68 @@
 "use strict";
 
-const form = document.forms.namedItem("registration");
+const form = document.forms.namedItem("select-radio");
+const submit = document.querySelector('#submit');
+const change = document.querySelector('#change');
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    const name = form.name.value;
-    const password = form.password.value
+    const radio = form.radio;
 
-    const formData = new FormData(form);
 
-    formData.set("name", name);
-    formData.set("password", password);
+    submit.innerHTML = radio.value;
 
-    console.log(formData.has("name"));
-    console.log(formData.has("password"));
-
-    console.log(formData.get("name"));
-    console.log(formData.get("password"));
-
-    //DELETE
-    formData.delete("name")
-    console.log(formData.has("name"));
+    // console.log(radio.value);
+    // radio.forEach((res) => {
+    //     if (res.checked) {
+    //         console.log(res);
+    //     }
+    // });
 });
+
+form.radio.forEach(res => {
+    res.addEventListener('change', (event) => {
+        const { target } = event;
+
+        if (target.checked) {
+            change.innerHTML = target.value;
+        }
+
+    });
+});
+
+
+/* MANIPULANDO INPUT
+// const form = document.forms.namedItem("registration");
+ 
+//form.addEventListener("input", (event) => {
+//     event.preventDefault();
+
+//     const name = form.name.value;
+
+//     const newDivElement = document.createElement("div");
+//     newDivElement.innerText = name.toUpperCase();
+
+//     form.nextElementSibling.remove();
+//     form.after(newDivElement);
+});
+ */
+
+/* FORM E FORMDATA
+// const password = form.password.value
+
+// const formData = new FormData(form);
+
+// formData.set("name", name);
+// formData.set("password", password);
+
+// console.log(formData.has("name"));
+// console.log(formData.has("password"));
+
+// console.log(formData.get("name"));
+// console.log(formData.get("password"));
+
+// //DELETE
+// formData.delete("name")
+// console.log(formData.has("name")); 
+*/
