733061a97d24a1d8e8f5e45389f5e40b48d35187
Alexandre Ferreira <ferolialexandre@gmail.com>
2023-10-14 09:35:08 -0300

    Definindo getters and setters.

--- modulo_typescript/003-classes/001-class/index.ts
+++ modulo_typescript/003-classes/001-class/index.ts
@@ -1,18 +1,44 @@
 class Pessoa {
-    nome: string;
-    idade: number;
+    private _nome: string;
+    private _idade: number;
+    private _docNumber: number;
 
-    constructor(nome: string, idade: number) {
-        this.nome = nome;
-        this.idade = idade;
+    public get nome(): string {
+        return this._nome;
+    }
+
+    public get idade(): number {
+        return this._idade;
+    }
+
+    public get docNumber(): number {
+        return this._docNumber;
+    }
+
+    public set nome(value: string) {
+        this._nome = value;
+    }
+
+    public set idade(value: number) {
+        this._idade = value;
+    }
+
+    public set docNumber(value: number) {
+        this._docNumber = value;
+    }
+
+    constructor(nome: string, idade: number, docNumber: number) {
+        this._nome = nome;
+        this._idade = idade;
+        this._docNumber = docNumber;
     }
 
     getPessoa(): void {
-        const retorno = `Nome: ${this.nome} | idade: ${this.idade}`;
+        const retorno = `Nome: ${this._nome} | idade: ${this._idade} | documento: ${this._docNumber}`;
         console.log(retorno);
     }
 }
 
-const user = new Pessoa("Usuário da Silva", 31);
+const user = new Pessoa("Usuário da Silva", 31, 123456);
 user.getPessoa();
 
