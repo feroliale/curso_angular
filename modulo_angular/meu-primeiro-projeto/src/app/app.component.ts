160ebb15b14e33b9ed32bd44946d1ca600450607
Alexandre Ferreira <ferolialexandre@gmail.com>
2023-10-31 12:04:09 -0300

    Sneding values from son to father component

--- modulo_angular/meu-primeiro-projeto/src/app/app.component.ts
+++ modulo_angular/meu-primeiro-projeto/src/app/app.component.ts
@@ -4,34 +4,51 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
     <!-- <app-diretivas-estruturais></app-diretivas-estruturais>
+    
     <app-data-binding></app-data-binding> -->
     
     <!-- <app-diretivas-aributos>
-      <h1>Aulas de Diretivas - Atributos</h1>
+    
+    <h1>Aulas de Diretivas - Atributos</h1>
+    
       <h3>Final da Aula</h3>
+    
     </app-diretivas-aributos>
     
     <app-diretivas-aributos>
-      <h1>Cópia de diretiva</h1>
+    
+    <h1>Cópia de diretiva</h1>
+    
       <h3>Final da cópia</h3>
+    
     </app-diretivas-aributos> -->
 
     <!-- <app-new-component></app-new-component> -->
     
     <!-- <app-input [contador]="addValue"></app-input>
+    
     <br>
+    
     <button (click)="add()">Add</button> -->
+    
+    <ng-template [ngIf]="getDados">
+      <h1>{{getDados.nome}}</h1>
+      <h2>{{getDados.idade}}</h2>
+      <hr>
+    </ng-template>
 
-    <app-output></app-output>
+    <app-output (enviarDados)="setDados($event)"></app-output>
 
     <router-outlet></router-outlet>
   `
 })
 
 export class AppComponent {
-  public destruir: boolean = true;
 
+  public destruir: boolean = true;
   public addValue: number = 0;
+
+  public getDados: { nome: string, idade: number } | undefined;
 
   public add() {
     this.addValue += 1;
@@ -39,5 +56,9 @@ export class AppComponent {
 
   public destruirComponent() {
     this.destruir = false;
+  }
+
+  public setDados(event: { nome: string, idade: number }) {
+    this.getDados = event;
   }
 }
